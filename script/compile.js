ba1ad86eacf154e1303ae07bae4f303590e33a70
Vishalkks <vishalkks@gmail.com>
2018-01-21 14:26:43 +0530

    final commit

--- script/compile.js
+++ script/compile.js
@@ -20,3 +20,24 @@ function render()
     document.getElementById("placeholder").innerHTML = xhr.responseText
   }
 }
+
+function publish(){
+
+var ascene = document.getElementsByTagName("a-scene")[0]
+var allcode = `<html>
+<head>
+<title>Experience</title>
+</head>
+<body>
+<a-scene>`
++ascene.innerHTML+
+`
+</a-scene><script src="http://aframe.io/releases/0.3.0/aframe.min.js"></script>
+<script src="http://cdn.rawgit.com/donmccurdy/aframe-extras/v2.5.2/dist/aframe-extras.js"></script>
+</body>
+</html>
+`
+console.log(allcode)
+
+
+}
