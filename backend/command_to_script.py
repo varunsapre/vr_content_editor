dc10015e9245ddb067bb4a934a4cec3e4f116b9b
orangeyash <yagarwal095@gmail.com>
2018-01-21 13:20:37 +0530

    added drag functionality

--- backend/command_to_script.py
+++ backend/command_to_script.py
@@ -22,7 +22,7 @@ def process(cmd,coord,pathVar):
 			cr = 'color = "black"'
 			if dicto[shape]["color"] != None:
 				cr = ' color = "' + dicto[shape]["color"] + '" '
-			liner = '<a-box position="'+' '.join(map(str,coord))+'" rotation="0 45 0"'+ cr +'shadow="" material="" geometry=""></a-box>'
+			liner = '<a-box position="'+' '.join(map(str,coord))+'" rotation="0 45 0"'+ cr +'shadow="" click-drag></a-box>'
 			#print(liner)
 
 		if "sphere" in shape:
@@ -32,7 +32,7 @@ def process(cmd,coord,pathVar):
 				cr = ' color = "' + dicto[shape]["color"] + '" '
 			if dicto[shape]["radius"] != None:
 				r = ' radius = "' + dicto[shape]["radius"] + '" '
-			liner = '<a-sphere position="'+' '.join(map(str,coord))+'"' + cr + r + 'shadow="" material="" geometry=""></a-sphere>'
+			liner = '<a-sphere position="'+' '.join(map(str,coord))+'"' + cr + r + 'shadow="" click-drag></a-sphere>'
 			#print(liner)
 
 		if "cylinder" in shape:
@@ -45,24 +45,64 @@ def process(cmd,coord,pathVar):
 				r = ' radius = "' + dicto[shape]["radius"] + '" '
 			if dicto[shape]["height"] != None:
 				h = ' height = "' + dicto[shape]["height"] + '" '
-			liner = '<a-cylinder position="'+' '.join(map(str,coord))+'"' + cr+r+h +'shadow="" material="" geometry=""></a-cylinder>'
+			liner = '<a-cylinder position="'+' '.join(map(str,coord))+'"' + cr+r+h +'shadow="" click-drag></a-cylinder>'
 			#print(liner)
 		store[shape] = liner
 
 	output = open('data.pkl', 'wb')
 	pickle.dump(store, output)
 	output.close()
+	'''//////////////////////////update/////////'''
 
 	if cmd.split()[0].lower() == "update":
-		print(return_value(cmd))
+		with open('data.pkl', 'rb') as pickle_file:
+			store = pickle.load(pickle_file)
+		pickle_file.close()
 
+		dicto = return_value(cmd)
+
+
+		shape = list(dicto.keys())[0]
+		liner_old = store[shape]
+		tok = liner_old.split()
+		if "cube" in shape:
+			cr = 'color = ' + tok[tok.index('color')+2] + ''
+			if dicto[shape]["color"] != None:
+				cr = ' color = "' + dicto[shape]["color"] + '" '
+			liner = '<a-box position="'+' '.join(map(str,coord))+'" rotation="0 45 0"'+ cr +'shadow="" click-drag ></a-box>'
+			#print(liner)
+
+		if "sphere" in shape:
+			cr = 'color = ' + tok[tok.index('color')+2] + ''
+			r = 'radius = ' + tok[tok.index('radius')+2] + ''
+			if dicto[shape]["color"] != None:
+				cr = ' color = "' + dicto[shape]["color"] + '" '
+			if dicto[shape]["radius"] != None:
+				r = ' radius = "' + dicto[shape]["radius"] + '" '
+			liner = '<a-sphere position="'+' '.join(map(str,coord))+'"' + cr + r + 'shadow="" click-drag></a-sphere>'
+			#print(liner)
+
+		if "cylinder" in shape:
+			cr = 'color = ' + tok[tok.index('color')+2] + ''
+			r = 'radius = ' + tok[tok.index('radius')+2] + ''
+			h = 'height = ' + tok[tok.index('radius')+2] + ''
+			if dicto[shape]["color"] != None:
+				cr = ' color = "' + dicto[shape]["color"] + '" '
+			if dicto[shape]["radius"] != None:
+				r = ' radius = "' + dicto[shape]["radius"] + '" '
+			if dicto[shape]["height"] != None:
+				h = ' height = "' + dicto[shape]["height"] + '" '
+			liner = '<a-cylinder position="'+' '.join(map(str,coord))+'"' + cr+r+h +'shadow="" click-drag></a-cylinder>'
+			#print(liner)
+		store[shape] = liner
+		#print(store)
 
 	if cmd.split()[0].lower() == "delete":
 		shape_name = return_value_delete(cmd)
 		with open('data.pkl', 'rb') as pickle_file:
 			store = pickle.load(pickle_file)
 		pickle_file.close()
-
+	
 		del store[shape_name]
 
 		output = open('data.pkl', 'wb')
@@ -76,12 +116,15 @@ def process(cmd,coord,pathVar):
 		#print(keys)
 	#print(string)
 
+
 	header = '<canvas class="a-canvas" data-aframe-canvas="true" width="300" height="150"></canvas><a-camera universal-controls wasd-controls><a-cursor></a-cursor></a-camera>'
 	footer = '<a-sky src = "' + pathVar + '" ></a-sky>'
-
+	#print(string)
 	body = header + string + footer
 	#print(body)
 	return body
 
 
-#process("Insert cube three color red",[1,1,1],"1.jpg")
+#process("Insert sphere one color red radius 5",[1,1,1],"1.jpg")
+#process("Insert cylinder one color red radius 5 height 3",[1,1,1],"1.jpg")
+#process("Update sphere one radius 2",[1,1,1],"1.jpg")
